f2b0c3bcd95d7d7d31034c4ed6ae564491adb5f1
swashy stable <roland.macdavid@gmail.com>
2017-03-01 11:43:11 -0500

    let's try using gawk to replace a value inline

--- update-ip.sh
+++ update-ip.sh
@@ -3,12 +3,12 @@
 #Get the latest IP
 newIP=$(tail -n 1 /home/swashy/ip.txt)
 # Get the line number where our A record IP and Serial is
-IPLine=$(grep -n 'FINDME' /etc/nsd/zones/somewhere.mudcrabz.net.conf |  cut -d: -f1)
-SerialLine=$(grep -n 'Serial' /etc/nsd/zones/somewhere.mudcrabz.net.conf |  cut -d: -f1)
-
+iPLine=$(grep -n 'FINDME' /etc/nsd/zones/somewhere.mudcrabz.net.conf |  cut -d: -f1)
+serialLine=$(grep -n 'Serial' /etc/nsd/zones/somewhere.mudcrabz.net.conf |  cut -d: -f1)
+serialNumber=$(grep -e 'Serial' /etc/nsd/zones/somewhere.mudcrabz.net.conf | cut -d\; -f1)
 
 #Insert IP line into number into awk, pipe the line into grep to get the IP.
-currentIP=$(awk "NR==$IPLine" /etc/nsd/zones/somewhere.mudcrabz.net.conf | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+currentIP=$(awk "NR==$iPLine" /etc/nsd/zones/somewhere.mudcrabz.net.conf | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
 
 #https://www.gnu.org/software/bash/manual/bashref.html#Conditional-Constructs
 if [ "$newIP" == "$currentIP" ]; then
@@ -18,6 +18,10 @@ if [ "$newIP" == "$currentIP" ]; then
     #awk "NR==$IPLine { sub("AAA", "BBB") }"
     # TODO Still trying to figure out how to replace a string in a specific line, but just going by IP now.
     sed -i.bak "s/$currentIP/$newIP/" /etc/nsd/zones/somewhere.mudcrabz.net.conf
+    serialNumberIncremented=$(($serialNumber+1))
+    
+    gawk -i inplace "NR==$serialLine { sub("$serialNumber", "$serialNumberIncremented") }; { print }" /etc/nsd/zones/somewhere.mudcrabz.net.conf
+    #sed -i.bak "s/$serialNumber/$serialNumberIncremented/" /etc/nsd/zones/somewhere.mudcrabz.net.conf
 fi
 
 
