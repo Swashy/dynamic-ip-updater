a5e2e6cf97a1360a4fd0ba97186762ddbac5c25b
swashy stable <roland.macdavid@gmail.com>
2017-03-02 21:20:21 -0500

    gawk doesn't seem to be able to replace strings based on a certain line, so I'm sticking with finding things to replace through marker comments and then replacing with sed

--- update-ip.sh
+++ update-ip.sh
@@ -2,9 +2,8 @@
 
 #Get the latest IP
 newIP=$(tail -n 1 /home/swashy/ip.txt)
-# Get the line number where our A record IP and Serial is
+# Get the line number where our A record IP is.
 iPLine=$(grep -n 'FINDME' /etc/nsd/zones/somewhere.mudcrabz.net.conf |  cut -d: -f1)
-serialLine=$(grep -n 'Serial' /etc/nsd/zones/somewhere.mudcrabz.net.conf |  cut -d: -f1)
 serialNumber=$(grep -e 'Serial' /etc/nsd/zones/somewhere.mudcrabz.net.conf | cut -d\; -f1)
 
 #Insert IP line into number into awk, pipe the line into grep to get the IP.
@@ -15,13 +14,13 @@ if [ "$newIP" == "$currentIP" ]; then
     echo "IPs are the same!, \n newIP: $newIP \n currentIP: $currentIP"
   else
     echo "IP's are not the same!!, \n newIP: $newIP \n currentIP: $currentIP"
-    #awk "NR==$IPLine { sub("AAA", "BBB") }"
-    # TODO Still trying to figure out how to replace a string in a specific line, but just going by IP now.
-    sed -i.bak "s/$currentIP/$newIP/" /etc/nsd/zones/somewhere.mudcrabz.net.conf
+
+    sed -i "/FINDME/ s/$currentIP/$newIP/" /etc/nsd/zones/somewhere.mudcrabz.net.conf
     serialNumberIncremented=$(($serialNumber+1))
     
-    gawk -i inplace "NR==$serialLine { sub("$serialNumber", "$serialNumberIncremented") }; { print }" /etc/nsd/zones/somewhere.mudcrabz.net.conf
+    #gawk -i inplace -v one="$serialLine" -v two="$serialNumber" -v three="$SerialNumberIncremented" 'NR==one { sub("two", "three") }; { print }' /etc/nsd/zones/somewhere.mudcrabz.net.conf
     #sed -i.bak "s/$serialNumber/$serialNumberIncremented/" /etc/nsd/zones/somewhere.mudcrabz.net.conf
+    sed -i "/Serial/ s/$SerialNumber/$SerialNumberIncremented/" /etc/nsd/zones/somewhere.mudcrabz.net.conf
 fi
 
 
