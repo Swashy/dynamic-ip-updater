1e2e747cfc4cbe00ea636fefb1a33b90240b90de
swashy stable <roland.macdavid@gmail.com>
2017-03-02 22:45:46 -0500

    actually get the serial incrementer working

--- update-ip.sh
+++ update-ip.sh
@@ -5,22 +5,26 @@ newIP=$(tail -n 1 /home/swashy/ip.txt)
 # Get the line number where our A record IP is.
 iPLine=$(grep -n 'FINDME' /etc/nsd/zones/somewhere.mudcrabz.net.conf |  cut -d: -f1)
 serialNumber=$(grep -e 'Serial' /etc/nsd/zones/somewhere.mudcrabz.net.conf | cut -d\; -f1)
+#strip trailing and leading whitespace
+serialNumber="$(echo -e "${serialNumber}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 
+echo "Serial number is $serialNumber"
 #Insert IP line into number into awk, pipe the line into grep to get the IP.
 currentIP=$(awk "NR==$iPLine" /etc/nsd/zones/somewhere.mudcrabz.net.conf | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
 
 #https://www.gnu.org/software/bash/manual/bashref.html#Conditional-Constructs
 if [ "$newIP" == "$currentIP" ]; then
-    echo "IPs are the same!, \n newIP: $newIP \n currentIP: $currentIP"
+    echo -e "IPs are the same! \n$currentIP"
   else
-    echo "IP's are not the same!!, \n newIP: $newIP \n currentIP: $currentIP"
+    echo -e "IP's are not the same!! \nnewIP: $newIP \ncurrentIP: $currentIP"
 
-    sed -i "/FINDME/ s/$currentIP/$newIP/" /etc/nsd/zones/somewhere.mudcrabz.net.conf
+    # These sed command's first field "/FINDME/" and "/[0-9]{1-10}/" will go to the line with this match
+    # the arg after replaces a finding.
+    sed -i.bak "/FINDME/ s/$currentIP/$newIP/" /etc/nsd/zones/somewhere.mudcrabz.net.conf
     serialNumberIncremented=$(($serialNumber+1))
-    
-    #gawk -i inplace -v one="$serialLine" -v two="$serialNumber" -v three="$SerialNumberIncremented" 'NR==one { sub("two", "three") }; { print }' /etc/nsd/zones/somewhere.mudcrabz.net.conf
-    #sed -i.bak "s/$serialNumber/$serialNumberIncremented/" /etc/nsd/zones/somewhere.mudcrabz.net.conf
-    sed -i "/Serial/ s/$SerialNumber/$SerialNumberIncremented/" /etc/nsd/zones/somewhere.mudcrabz.net.conf
+    echo "New serial will be $serialNumberIncremented"
+    sed -i.bak "/Serial/ s/[0-9]/$serialNumberIncremented/" /etc/nsd/zones/somewhere.mudcrabz.net.conf
+    nsd-control reload
 fi
 
 
